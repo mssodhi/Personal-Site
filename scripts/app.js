72eb4d6b5e7fcfeb2cc97b29f367c4befe77ff28
Manu SOdhi <mssodhi@ucdavis.edu>
2015-10-25 21:41:10 -0700

    adding angular

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('app', ['ngRoute', 'ngResource'])
+    .config(function ($routeProvider, $httpProvider) {
+        $httpProvider.interceptors.push('AuthenticationInterceptor');
+        $routeProvider
+            .when('/', {
+                templateUrl: 'scripts/controllers/home/home.html',
+                controller: 'HomeCtrl',
+                controllerAs: 'ctrl'
+            })
+            // Route for unauthorized access (When permission is not given to visit a page)
+            .when('/deny',
+            {
+                templateUrl: 'resources/scripts/controllers/deny/deny.html',
+                caseInsensitiveMatch: true
+            })
+            .otherwise({
+                redirectTo: '/deny'
+            })
+        ;
+    });
+
+angular.module('app').run(function ($window, $location) {
+    //if($window.sessionStorage.getItem('originalUrl')) {
+    //    $location.url($window.sessionStorage.getItem('originalUrl'));
+    //    delete $window.sessionStorage.originalUrl;
+    //}
+});
