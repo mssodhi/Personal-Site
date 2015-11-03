60f280658624c7d935b11fa7d0fc22aa692b0644
Manu SOdhi <mssodhi@ucdavis.edu>
2015-11-03 02:57:32 -0800

    Landing page refactor

--- scripts/controllers/home/home.js
+++ scripts/controllers/home/home.js
@@ -4,7 +4,7 @@ angular.module('app').controller('HomeCtrl', function () {
     var ctrl = this;
 
     ctrl.messages = [
-        {text:"Standard Message"},
+        {text:"Standard Message", type: 'no message'},
         {text:"Success Message!", type:"success"},
         {text:"Alert Message!", type : "alert"},
         {text:"secondary message...", type : "secondary"}
