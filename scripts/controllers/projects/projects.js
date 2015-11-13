da90fba1a76c93df7cfdac7f7e7e46a524da0b72
Manu SOdhi <mssodhi@ucdavis.edu>
2015-11-12 20:24:16 -0800

    Added time stamp and fixed landing page text

--- scripts/controllers/projects/projects.js
+++ scripts/controllers/projects/projects.js
@@ -20,7 +20,8 @@ angular.module('app').controller('ProjectsCtrl', ['$scope', '$modal', '$http', f
         // Get the date from github and convert it Date String
         var ne = Date.parse(project.updated_at);
         var date = new Date(ne);
-        $scope.currentProject.date = date.toLocaleString();
+        $scope.currentProject.date = date.toDateString();
+        $scope.currentProject.time = date.toLocaleTimeString();
 
         // Open the actual modal.
         ctrl.showModal();
