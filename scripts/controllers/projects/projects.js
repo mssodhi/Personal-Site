ee5e414fc81a91754a9bee8b52873e5661764867
Manu SOdhi <mssodhi@ucdavis.edu>
2015-11-12 20:16:57 -0800

    Date added to projects modal

--- scripts/controllers/projects/projects.js
+++ scripts/controllers/projects/projects.js
@@ -14,14 +14,19 @@ angular.module('app').controller('ProjectsCtrl', ['$scope', '$modal', '$http', f
 
 
     ctrl.openModal = function (project) {
+        // Set the current project
         $scope.currentProject = project;
+
+        // Get the date from github and convert it Date String
         var ne = Date.parse(project.updated_at);
         var date = new Date(ne);
-        $scope.currentProject.date = date.toDateString();
-        ctrl.showForm();
+        $scope.currentProject.date = date.toLocaleString();
+
+        // Open the actual modal.
+        ctrl.showModal();
     };
 
-    ctrl.showForm = function () {
+    ctrl.showModal = function () {
         var modalInstance = $modal.open({
             templateUrl: 'scripts/controllers/projects/modal-form.html',
             controller: ModalInstanceCtrl,
