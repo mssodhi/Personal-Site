91fed3480eadc07687dcfe6f06558ffb008cd34f
Manu SOdhi <mssodhi@ucdavis.edu>
2015-11-12 22:10:45 -0800

    Removed outside dependencies of ui-bootstrap. code cleanup

--- scripts/controllers/projects/projects.js
+++ scripts/controllers/projects/projects.js
@@ -37,39 +37,8 @@ angular.module('app').controller('ProjectsCtrl', ['$scope', '$modal', '$http', f
 
     var ModalInstanceCtrl = function ($scope, $modalInstance) {
         $scope.cancel = function () {
-            $modalInstance.dismiss('cancel');
+            $modalInstance.dismiss();
         };
     };
-
-
-    //ctrl.googleChartsStuff = function () {
-    //        setTimeout(function(){google.load('visualization', '1', {'callback':'', 'packages':['corechart']})}, 0);
-    //
-    //        //google.load("visualization", "1", {packages:["corechart"]});
-    //        google.setOnLoadCallback(drawChart);
-    //        function drawChart() {
-    //            var data = google.visualization.arrayToDataTable([
-    //                ['Age', 'Weight'],
-    //                [ 8,      12],
-    //                [ 4,      5.5],
-    //                [ 11,     14],
-    //                [ 4,      5],
-    //                [ 3,      3.5],
-    //                [ 6.5,    7]
-    //            ]);
-    //
-    //            var options = {
-    //                title: 'Age vs. Weight comparison',
-    //                hAxis: {title: 'Age', minValue: 0, maxValue: 15},
-    //                vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
-    //                legend: 'none'
-    //            };
-    //
-    //            var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-    //
-    //            chart.draw(data, options);
-    //        }
-    //};
-
 
 }]);
