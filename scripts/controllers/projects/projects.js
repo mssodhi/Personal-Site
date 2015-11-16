852fa68a439bb660cba096acbbee0a6e9f2a0687
Manu SOdhi <mssodhi@ucdavis.edu>
2015-11-16 00:27:14 -0800

    Fixed the updated date on projects page. Now it shows when the last push was.

--- scripts/controllers/projects/projects.js
+++ scripts/controllers/projects/projects.js
@@ -18,7 +18,7 @@ angular.module('app').controller('ProjectsCtrl', ['$scope', '$modal', '$http', f
         $scope.currentProject = project;
 
         // Get the date from github and convert it Date String
-        var ne = Date.parse(project.updated_at);
+        var ne = Date.parse(project.pushed_at);
         var date = new Date(ne);
         $scope.currentProject.date = date.toDateString();
         $scope.currentProject.time = date.toLocaleTimeString();
